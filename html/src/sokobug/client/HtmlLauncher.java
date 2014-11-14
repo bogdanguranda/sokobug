1d0dfde9d9eafb515d928baaeff777efc1d913c3
Bogdan Guranda <bogdanguranda@gmail.com>
2014-11-14 16:35:51 +0200

    Initial project commit

---
+++ html/src/sokobug/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package sokobug.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import sokobug.Application;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Application();
+        }
+}
