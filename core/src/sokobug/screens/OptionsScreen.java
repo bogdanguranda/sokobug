c3d6e80462c24936a2134e0149eae820858a86d4
Bogdan Guranda <bogdanguranda@gmail.com>
2015-02-26 20:39:55 +0200

    Back button for ChooseLevelScreen and OptionsScreen.

--- core/src/sokobug/screens/OptionsScreen.java
+++ core/src/sokobug/screens/OptionsScreen.java
@@ -1,24 +1,44 @@
 package sokobug.screens;
 
 import sokobug.Sokobug;
+import sokobug.domain.MenuButton;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class OptionsScreen implements Screen, InputProcessor {
 
 	Sokobug game;
     private BitmapFont font;
+    private Stage stage;
+    private MenuButton backToMenu;
+    private InputMultiplexer multiplexer;
 	
 	public OptionsScreen(Sokobug myGame) {
 		game = myGame;
+		stage = new Stage(game.viewport);
+		multiplexer = new InputMultiplexer();
 		
 		font = game.assetManager.get("fonts/Papyrus.fnt", BitmapFont.class);
+		game.assetManager.load("skins/uiskin.atlas", TextureAtlas.class);
+		game.assetManager.load("skins/uiskin.json", Skin.class,
+				new SkinLoader.SkinParameter("skins/uiskin.atlas"));
+		game.assetManager.finishLoading();
+		
+		backToMenu = new MenuButton(game, "BackToMenu", game.assetManager.get(
+				"skins/uiskin.json", Skin.class));
+		
+		backToMenu.setPosition(0, 0);
 	}
 	
 	@Override
@@ -35,11 +55,16 @@ public class OptionsScreen implements Screen, InputProcessor {
 		game.batch.begin();
 		font.draw(game.batch, text, (game.VIRTUAL_WIDTH / 2) - font.getBounds(text).width / 2, (game.VIRTUAL_HEIGHT / 2) - font.getBounds(text).height / 2);
 		game.batch.end();
+		
+		stage.act();
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose() {
-		
+		stage.dispose();
+		game.assetManager.unload("skins/uiskin.atlas");
+		game.assetManager.unload("skins/uiskin.json");
 	}
 
 	@Override
@@ -51,7 +76,10 @@ public class OptionsScreen implements Screen, InputProcessor {
 
 	@Override
 	public void show() {
-		Gdx.input.setInputProcessor(this);
+		stage.addActor(backToMenu);
+		multiplexer.addProcessor(stage);
+		multiplexer.addProcessor(this);
+		Gdx.input.setInputProcessor(multiplexer);
 	}
 
 	@Override
