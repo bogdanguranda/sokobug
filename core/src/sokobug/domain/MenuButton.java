34a23766e1a0015f26c8cee2021115734d648013
Corvin Tiperciuc <cuvidk@yahoo.ro>
2015-02-26 16:16:53 +0200

    Buttons behavior changes mostly *Removes the button classes and the menuButton folder *Creates MenuButton class that behaves accordingly to the buttonType variable *Sets the corresponding screen accordingly to which button was pressed *MainMenu disposal is no longer applied when setting another screen.  This was done because pressing ESC keys in ChooseLevelScreen, CreditsScreen,  OptionsScreen would have resulted in an error because MainMenuScreen will  no longer exist.

---
+++ core/src/sokobug/domain/MenuButton.java
@@ -0,0 +1,60 @@
+package sokobug.domain;
+
+import sokobug.Sokobug;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MenuButton extends TextButton {
+	private String buttonType;
+	private Sokobug game;
+
+	public MenuButton(Sokobug game, String buttonType, Skin skin) {
+		super(buttonType, skin);
+		this.buttonType = buttonType;
+		this.game = game;
+
+		try {
+			manageEvents();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	private void manageEvents() throws Exception {
+		if (buttonType.compareTo("Play") == 0)
+			this.addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					game.setScreen(game.chooseLevelScreen);
+				}
+			});
+		else if (buttonType.compareTo("Options") == 0)
+			this.addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					game.setScreen(game.optionsScreen);
+				}
+			});
+		else if (buttonType.compareTo("Credits") == 0)
+			this.addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					game.setScreen(game.creditsScreen);
+				}
+			});
+		else if (buttonType.compareTo("Exit") == 0)
+			this.addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					Gdx.app.exit();
+				}
+			});
+		else
+			throw new Exception("Error: Wrong button type...");
+	}
+}
