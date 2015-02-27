aae8345b953c95fba9f96e36953d01045bfb6a20
Corvin Tiperciuc <cuvidk@yahoo.ro>
2015-02-27 12:27:27 +0200

    MainMenu - keyboard + mouse control *Adds focused buttons states as booleans in MainMenuScreen. *Creates buttonPressed and focusedButton skins. *Adds keyboard control over the main menu. *Handles the navigation between widgets using the focused buttons states,  the graphic effects being handled by setting the current focused button's  style using focusedButton skin. *Modifies MenuButton behavior so that when the mouse is over an widget,  all the buttons will be defocused. *Modifies the navigation of menu so that keyboard navigation is posible  only when mouse doesn't focus any widget. *Uses InputMultiplexer so input can be perceived by more than 1 class. *Sets the text of the buttons more centered then before.

--- core/src/sokobug/screens/MainMenuScreen.java
+++ core/src/sokobug/screens/MainMenuScreen.java
@@ -4,6 +4,9 @@ import sokobug.Sokobug;
 import sokobug.domain.MenuButton;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.graphics.Color;
@@ -14,38 +17,82 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
-public class MainMenuScreen implements Screen {
-	private Sokobug game;
+public class MainMenuScreen implements Screen, InputProcessor {
 	private Table table;
 	private Stage stage;
 	private Sprite background;
+	private InputMultiplexer multiplexer;
 	private MenuButton play;
 	private MenuButton options;
 	private MenuButton credits;
 	private MenuButton exit;
 
+	public Sokobug game;
+	public Boolean isPlayFocused;
+	public Boolean isOptionFocused;
+	public Boolean isCreditsFocused;
+	public Boolean isExitFocused;
+	public Skin focusedButtonSkin;
+	public Skin normalButtonSkin;
+
 	public MainMenuScreen(Sokobug game) {
 		this.game = game;
 		table = new Table();
 		stage = new Stage(game.viewport);
+		multiplexer = new InputMultiplexer();
 
 		game.assetManager.load("MainMenuScreen.png", Texture.class);
 		game.assetManager.load("skins/uiskin.atlas", TextureAtlas.class);
 		game.assetManager.load("skins/uiskin.json", Skin.class,
 				new SkinLoader.SkinParameter("skins/uiskin.atlas"));
+		game.assetManager.load("skins/focusedButton.json", Skin.class,
+				new SkinLoader.SkinParameter("skins/focusedButton.atlas"));
 		game.assetManager.finishLoading();
 
 		background = new Sprite(game.assetManager.get("MainMenuScreen.png",
 				Texture.class));
-		play = new MenuButton(game, "Play", game.assetManager.get(
+		play = new MenuButton(this, "Play", game.assetManager.get(
 				"skins/uiskin.json", Skin.class));
-		options = new MenuButton(game, "Options", game.assetManager.get(
+		options = new MenuButton(this, "Options", game.assetManager.get(
 				"skins/uiskin.json", Skin.class));
-		credits = new MenuButton(game, "Credits", game.assetManager.get(
+		credits = new MenuButton(this, "Credits", game.assetManager.get(
 				"skins/uiskin.json", Skin.class));
-		exit = new MenuButton(game, "Exit", game.assetManager.get(
+		exit = new MenuButton(this, "Exit", game.assetManager.get(
 				"skins/uiskin.json", Skin.class));
+
+		focusedButtonSkin = game.assetManager.get("skins/focusedButton.json",
+				Skin.class);
+		normalButtonSkin = game.assetManager.get("skins/uiskin.json",
+				Skin.class);
+		
+		table.add(play).row();
+		table.add(options).row();
+		table.add(credits).row();
+		table.add(exit).row();
+		table.setFillParent(true);
+		// table.setDebug(true);
+
+		stage.addActor(table);
+	}
+
+	public MenuButton getFocusedButton() {
+		if (isOptionFocused)
+			return options;
+		else if (isCreditsFocused)
+			return credits;
+		else if (isExitFocused)
+			return exit;
+		else
+			return play;
+	}
+	
+	public void defocusButtons() {
+		isPlayFocused = false;
+		isOptionFocused = false;
+		isCreditsFocused = false;
+		isExitFocused = false;
 	}
 
 	@Override
@@ -74,21 +121,23 @@ public class MainMenuScreen implements Screen {
 
 	@Override
 	public void show() {
-		table.add(play).row();
-		table.add(options).row();
-		table.add(credits).row();
-		table.add(exit).row();
-		table.setFillParent(true);
-		// table.setDebug(true);
+		//Each time MainMenuScreen is set again, the focused button
+		//is set to none.
+		defocusButtons();
 
-		stage.addActor(table);
+		play.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+		options.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+		credits.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+		exit.setStyle(normalButtonSkin.get(TextButtonStyle.class));
 
-		Gdx.input.setInputProcessor(stage);
+		multiplexer.addProcessor(stage);
+		multiplexer.addProcessor(this);
+		Gdx.input.setInputProcessor(multiplexer);
 	}
 
 	@Override
 	public void hide() {
-		
+
 	}
 
 	@Override
@@ -107,5 +156,125 @@ public class MainMenuScreen implements Screen {
 		game.assetManager.unload("MainMenuScreen.png");
 		game.assetManager.unload("skins/uiskin.atlas");
 		game.assetManager.unload("skins/uiskin.json");
+		game.assetManager.unload("skins/focusedButton.json");
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		//Reacting only if mouse is not over a menu widget
+		if (!play.isOver() && !options.isOver() && !credits.isOver()
+				&& !exit.isOver()) {
+			if (keycode == Input.Keys.DOWN) {
+				if (isPlayFocused) {
+					isPlayFocused = false;
+					isOptionFocused = true;
+					play.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+					options.setStyle(focusedButtonSkin
+							.get(TextButtonStyle.class));
+				} else if (isOptionFocused) {
+					isOptionFocused = false;
+					isCreditsFocused = true;
+					options.setStyle(normalButtonSkin
+							.get(TextButtonStyle.class));
+					credits.setStyle(focusedButtonSkin
+							.get(TextButtonStyle.class));
+				} else if (isCreditsFocused) {
+					isCreditsFocused = false;
+					isExitFocused = true;
+					credits.setStyle(normalButtonSkin
+							.get(TextButtonStyle.class));
+					exit.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				} else if (isExitFocused) {
+					isExitFocused = false;
+					isPlayFocused = true;
+					exit.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+					play.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				} else {
+					isPlayFocused = true;
+					play.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				}
+				return true;
+
+			} else if (keycode == Input.Keys.UP) {
+				if (isPlayFocused) {
+					isPlayFocused = false;
+					isExitFocused = true;
+					play.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+					exit.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				} else if (isExitFocused) {
+					isExitFocused = false;
+					isCreditsFocused = true;
+					exit.setStyle(normalButtonSkin.get(TextButtonStyle.class));
+					credits.setStyle(focusedButtonSkin
+							.get(TextButtonStyle.class));
+				} else if (isCreditsFocused) {
+					isCreditsFocused = false;
+					isOptionFocused = true;
+					credits.setStyle(normalButtonSkin
+							.get(TextButtonStyle.class));
+					options.setStyle(focusedButtonSkin
+							.get(TextButtonStyle.class));
+				} else if (isOptionFocused) {
+					isOptionFocused = false;
+					isPlayFocused = true;
+					options.setStyle(normalButtonSkin
+							.get(TextButtonStyle.class));
+					play.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				} else {
+					isExitFocused = true;
+					exit.setStyle(focusedButtonSkin.get(TextButtonStyle.class));
+				}
+				return true;
+
+			}
+		}
+
+		if (keycode == Input.Keys.ENTER) {
+			if (isPlayFocused)
+				game.setScreen(game.chooseLevelScreen);
+			else if (isOptionFocused)
+				game.setScreen(game.optionsScreen);
+			else if (isCreditsFocused)
+				game.setScreen(game.creditsScreen);
+			else if (isExitFocused)
+				Gdx.app.exit();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
