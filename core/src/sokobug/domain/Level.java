d1429c21dead2b83981cc5caf54917efcb640f5c
Bogdan Guranda <bogdanguranda@gmail.com>
2015-03-03 12:38:58 +0200

    Defined the Level class's arhitecture and basic functionality.

--- core/src/sokobug/domain/Level.java
+++ core/src/sokobug/domain/Level.java
@@ -1,6 +1,12 @@
 package sokobug.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 import sokobug.Sokobug;
 
@@ -9,15 +15,68 @@ public class Level implements InputProcessor{
 	private Sokobug game;
 	public int levelNumber;
 	
+	private static final int LABYRINTH_ROWS = 9;
+	private static final int LABYRINTH_COLUMNS = 16;
+	
+	private String[][] labyrinth = new String[LABYRINTH_ROWS][LABYRINTH_COLUMNS];
+	private int bugPosLine;
+	private int bugPosColumn;
+	private Map<String, Sprite> labyrinthSprites = new HashMap<String, Sprite>();
+	
+	private int vaseNumber; // equal with spots
+	private int vaseOnSpot = 0;
+	
+	public static int MOVE_NONE = 0;
+	public static int MOVE_UP = 1;
+	public static int MOVE_RIGHT = 2;
+	public static int MOVE_DOWN = 3;
+	public static int MOVE_LEFT = 4;
+	
+	private int movingDirection = MOVE_NONE;
+	
 	public Level(Sokobug game) {
 		this.game = game;
+		
+		labyrinthSprites.put("F", new Sprite(game.assetManager.get("ingame/free.png", Texture.class)));
+		labyrinthSprites.put("W", new Sprite(game.assetManager.get("ingame/wall.png", Texture.class)));
+		labyrinthSprites.put("S", new Sprite(game.assetManager.get("ingame/spot.png", Texture.class)));
+		labyrinthSprites.put("V", new Sprite(game.assetManager.get("ingame/vase.png", Texture.class)));
+		labyrinthSprites.put("B", new Sprite(game.assetManager.get("ingame/bug.png", Texture.class)));
+	}
+
+	public boolean isMoving() {
+		if (movingDirection != MOVE_NONE)
+			return true;
+		return false;
+	}
+	
+	public Vector2 labyrinthSize() {
+		return new Vector2(LABYRINTH_COLUMNS * labyrinthSprites.get("F").getWidth(), LABYRINTH_ROWS * labyrinthSprites.get("F").getHeight());
 	}
 	
 	public void load(int levelNumber) {
 		this.levelNumber = levelNumber;
 		// load the level
 	}
-
+	
+	public void draw(int deltaTime) {
+		
+	}
+	
+	public void moveBug(int MOVE_DIRECTION, int deltaTime) { // collision detection should be here
+		
+	}
+	
+	public boolean isVictory() {
+		if (vaseNumber == vaseOnSpot)
+			return true;
+		return false;
+	}
+	
+	public boolean isGameOver() {
+		return false;
+	}
+	
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
