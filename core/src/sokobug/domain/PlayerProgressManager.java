729cc5ab47de67312919eef057616d0286c7a405
Bogdan Guranda <bogdanguranda@gmail.com>
2015-05-09 09:11:18 +0300

    PlayerProgressManager cleanup.

--- core/src/sokobug/domain/PlayerProgressManager.java
+++ core/src/sokobug/domain/PlayerProgressManager.java
@@ -2,18 +2,14 @@ package sokobug.domain;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Base64Coder;
-import com.badlogic.gdx.utils.Json;
 
 public class PlayerProgressManager {
 	private static PlayerProgressManager playerProgressManager = new PlayerProgressManager();
-	private String filePath;
 	private String prefFileName;
 	private PlayerProgress playerProgress;
 	
 	{
-		filePath = "data.bin";
 		prefFileName = "sokobug";
 		playerProgress = new PlayerProgress();
 		loadPlayerProgress();
@@ -56,24 +52,4 @@ public class PlayerProgressManager {
 		pref.flush();
 	}
 	
-//	private void loadPlayerProgress() {
-//		FileHandle file = Gdx.files.local(filePath);
-//		Json json = new Json();
-//		if (file.exists()) {
-//			String progressAsCode = file.readString();
-//			String progressAsText = Base64Coder.decodeString(progressAsCode);
-//			playerProgress = json.fromJson(PlayerProgress.class, progressAsText);
-//		}
-//		else {
-//			savePlayerProgress();
-//		}
-//	}
-//	
-//	public void savePlayerProgress() {
-//		FileHandle file = Gdx.files.local(filePath);
-//		Json json = new Json();
-//		String progressAsText = json.toJson(playerProgress);
-//		String progressAsCode = Base64Coder.encodeString(progressAsText);
-//		file.writeString(progressAsCode, false);
-//	}
 }
