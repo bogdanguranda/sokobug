6068d9edaf25eed20cc6b411346cbb3e9c1b2e23
Bogdan Guranda <bogdanguranda@gmail.com>
2015-05-24 17:25:34 +0300

    On android back button event dosent kill activity instantly, but pops quit and resume buttons to make sure the user didn't hit the button by mistake.

--- android/src/sokobug/android/AndroidLauncher.java
+++ android/src/sokobug/android/AndroidLauncher.java
@@ -1,6 +1,12 @@
 package sokobug.android;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -9,9 +15,40 @@ import sokobug.Sokobug;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Sokobug(), config);
+	}
+
+	@Override
+	public void onBackPressed() {
+
+		final Dialog dialog = new Dialog(this);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		LinearLayout linerLayout = new LinearLayout(this);
+		linerLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+		Button buttonQuit = new Button(this);
+		buttonQuit.setText("Quit");
+		buttonQuit.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		linerLayout.addView(buttonQuit);
+
+		Button buttonResume = new Button(this);
+		buttonResume.setText("Resume");
+		buttonResume.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		linerLayout.addView(buttonResume);
+
+		dialog.setContentView(linerLayout);
+		dialog.show();
 	}
 }
